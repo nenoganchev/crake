09ee10d421bb91cf597d92341f9d8d94ce619edd
Neno Ganchev <neno.ganchev@tiger-technology.com>
2012-01-17 09:41:39 +0200

    First draft of the DSL.

---
+++ example_usage.rb
@@ -0,0 +1,57 @@
+library 'tdlib', :type => :static do
+
+  target_land :user # executes in user mode or kernel mode?
+
+  type :static # this way instead of the hash in the library call above? or support both ways?
+
+  # list the source files
+  source '../target_driver/string_pack.c'
+  source './target_driver.cpp'
+  source './scsi_disk.cpp'
+  source './log.cpp'
+
+  # list the necessary defines
+  define '_LIB'
+  define 'OS_64BIT' if config[:target_arch] == :intel64
+
+  # list include directories
+  include_dir "#{loki_dir}/include"
+  include_dir "#{tomcrypt_dir}/src/headers"
+  include_dir "#{path_to_top}/licensing/hasp/include"
+
+  # list the required libraries. note that they are used not when building the library, but when building the
+  # executables which link to this library
+  link "Advapi32.lib"
+  link "#{tomcrypt_dir}/#{config[:target_arch]}/user/tomcrypt.lib"
+
+  # instruct the linker where to look for libraries
+  library_dir "#{win_sdk_dir}/lib/#{config[:target_arch]}"
+end
+
+
+executable 'tdsvc' do
+  # target_land :user is implied
+
+  source './settings.cpp'
+  source './service.cpp'
+  source './web_server.cpp'
+
+  include_dir "#{loki_dir}/include"
+
+  link 'tdlib' # figure out that this library is defined here and link to its full path, wherever this is
+end
+
+
+driver 'scsitmd' do
+  # target_land :kernel is implied
+
+  source './scsitmd.c'
+  source './string_pack.c'
+
+  define 'OS_64BIT' if config[:target_arch] == :intel64
+  define 'XMALLOC=malloc_kernel' # is there a better way to define (name, value) pairs?
+
+  include_dir "#{tomcrypt_dir}/src/headers"
+
+  link "#{tomcrypt_dir}/#{config[:target_arch]}/kernel/tomcrypt.lib"
+end
