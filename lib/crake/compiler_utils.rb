7f2f97add40684647a6360a5453a05e3a6c8a621
Neno Ganchev <neno.ganchev@tiger-technology.com>
2012-02-10 01:36:04 +0200

    Implement compiler settings DSL in the configuration

--- lib/crake/compiler_utils.rb
+++ lib/crake/compiler_utils.rb
@@ -3,7 +3,28 @@ require 'crake/configuration'
 module CRake
   module CompilerUtils
     def compile(source_file, config, object_file)
-      puts "[CC] #{source_file} => #{object_file}"
+      compiler_flags = ["/nologo", "/errorReport:none", "/c"]
+
+      # add compiler options
+      {
+        :buffer_security_checks => { true => "/GS", false => "/GS-"},
+        :ignore_standard_include_paths => { true => "/X" },
+        :calling_convention => { :cdecl => "/Gd", :fastcall => "/Gr", :stdcall => "/Gz" },
+        :compiler_warnings_as_errors => { true => "/WX" },
+        :warning_level => Hash[ (0..4).map { |n| [n, "/W#{n}"] } ],
+        :debug_info_format => { :c7_compatible => "/Z7", :pdb => "/Zi", :pdb_edit_continue => "/ZI" },
+      }.each do |setting_name, setting_flags|
+        if config.has_key? setting_name
+          setting_flag = setting_flags[config[setting_name]]
+          compiler_flags << setting_flag if setting_flag  # setting_flag may be nil when a compiler flag doesn't have an opposite
+        end
+      end
+
+      # add include dirs
+
+      # add defines
+
+      puts "[CC] #{source_file} #{compiler_flags} -o #{object_file}"
     end
 
     def link(object_files, config, linked_file)
--- lib/crake/configuration.rb
+++ lib/crake/configuration.rb
@@ -22,6 +22,10 @@ module CRake
       @store[key] = value
     end
 
+    def has_key?(key_name)
+      @store.has_key? key_name
+    end
+
     #
     # DSL-implementing methods
     #
@@ -32,6 +36,48 @@ module CRake
 
     def int_dir(dir)
       @store[:int_dir] = dir
+    end
+
+    def buffer_security_checks(value)
+      @store[:buffer_security_checks] = map_to_bool(value)
+    end
+
+    def ignore_standard_include_paths(value)
+      @store[:ignore_standard_include_paths] = map_to_bool(value)
+    end
+
+    def calling_convention(conv_name)
+      raise "Incorrect calling convention '#{conv_name}'" if not [:cdecl, :fastcall, :stdcall].include? conv_name
+      @store[:calling_convention] = conv_name
+    end
+
+    def treat_compiler_warnings_as_errors(value)
+      @store[:compiler_warnings_as_errors] = map_to_bool(value)
+    end
+
+    def warning_level(level)
+      raise "Incorrect warning level #{level}" if not (0..4).include? level
+      @store[:warning_level] = level
+    end
+
+    def debug_info_format(format)
+      raise "Incorrect debug info format '#{format}'" if not [:c7_compatible, :pdb, :pdb_edit_continue].include? format
+      @store[:debug_info_format] = format
+    end
+
+    private
+
+    def map_to_bool(value)
+      value = value.to_sym if value.is_a? String
+      {
+        [:on,  true,  1] => true,
+        [:off, false, 0] => false
+      }.each { |mapped_values, bool_value| return bool_value if mapped_values.include? value }
+
+      # the value was not found in the predefined set, print a warning
+      caller_name = caller[0].match(/`(.*)'/)[1]
+      puts "[crake] warning: value '#{value}' passed to '#{caller_name}' could not be converted to bool, returning false"
+      false
     end
   end
 end
